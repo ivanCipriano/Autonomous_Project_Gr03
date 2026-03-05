a4f142ff5777da15c2017685990adbd09685ebb7
apiantonio <apiantonio.aa@gmail.com>
2026-03-05 16:18:55 +0100

    commenti obstacle evaluator

--- carla_behavior_agent/cognitive_modules/obstacle_evaluator.py
+++ carla_behavior_agent/cognitive_modules/obstacle_evaluator.py
@@ -4,10 +4,38 @@ from misc import compute_distance_from_center, is_within_distance, get_distance
 
 
 class StaticObstructionEvaluator(BaseEvaluator):
-    """Sostituisce lo Scenario 3: Evitamento ostacoli statici"""
+    """
+    Classe responsabile della valutazione e gestione degli ostacoli statici presenti nell'ambiente di simulazione.
+
+    Eredita da BaseEvaluator e costituisce un componente fondamentale del modulo di percezione e decisione del veicolo
+    autonomo. Il suo scopo principale è rilevare elementi stazionari di pericolo, come coni stradali o segnali di
+    avviso (traffic warnings), e calcolare le risposte comportamentali appropriate, quali l'evitamento dell'ostacolo
+    tramite ricalcolo della traiettoria globale (bypass) o l'applicazione di un offset laterale locale per la sicurezza.
+    Questa classe implementa la logica necessaria per risolvere scenari critici legati all'ostruzione della carreggiata.
+    """
 
     def _detect_static_hazards(self, current_waypoint, element_query, angle_interval=[0, 90]):
-        """Ex static_obstacle_manager: rileva coni e cartelli nell'ambiente"""
+        """
+        Rileva la presenza di pericoli statici specifici nell'area circostante l'ego-veicolo.
+
+        Il metodo interroga l'ambiente simulato (CARLA world) filtrando gli attori in base alla stringa di ricerca fornita.
+        Successivamente, isola gli elementi che si trovano entro un raggio di 20 metri e all'interno di un settore angolare
+        specifico rispetto alla posizione e all'orientamento dell'ego-veicolo. Gli ostacoli rilevati vengono ordinati per
+        distanza dal waypoint corrente. Presenta una logica di elaborazione differenziata per i coni stradali rispetto
+        ad altri elementi di intralcio generici.
+
+        Parametri:
+            current_waypoint (carla.Waypoint): Il waypoint in cui si trova attualmente l'ego-veicolo.
+            element_query (str): Stringa utilizzata per filtrare gli attori di CARLA (es. "static.prop.constructioncone").
+            angle_interval (list, opzionale): Intervallo angolare [min, max] in gradi per delimitare il campo visivo
+            della ricerca rispetto all'orientamento del veicolo. Il valore predefinito è [0, 90].
+
+        Ritorna:
+            tuple: Una tupla contenente tre elementi:
+            - bool: True se un ostacolo rilevante è stato individuato, False altrimenti.
+            - carla.Actor o None: L'oggetto CARLA rappresentante l'ostacolo più vicino, se presente.
+            - float: La distanza in metri dall'ostacolo (oppure -1 se non ci sono ostacoli validi).
+        """
         sys = self.core_system
         obs_list = sys._world.get_actors().filter(element_query)
         obs_list = [o for o in obs_list if is_within_distance(o.get_transform(), sys._vehicle.get_transform(), 20,
@@ -26,18 +54,34 @@ class StaticObstructionEvaluator(BaseEvaluator):
         )
 
     def evaluate(self, **kwargs):
+        """
+        Analizza l'ambiente circostante per identificare ostacoli statici ed esegue le manovre evasive necessarie.
+
+        Il metodo coordina la scansione dell'ambiente alla ricerca di cartelli di avviso e coni da cantiere. I risultati
+        della scansione vengono memorizzati nel sistema centrale per essere condivisi con altri valutatori della pipeline.
+        In caso di rilevamento di un cartello di avviso sulla traiettoria, calcola un percorso di evitamento (bypass).
+        Se l'evitamento non è fisicamente fattibile e l'ostacolo è in zona critica (< 6 metri), ordina un arresto
+        di emergenza. In presenza di coni da cantiere, applica dinamicamente uno scostamento laterale (offset) al planner
+        locale per aggirare i detriti, modulando la direzione in base all'appartenenza del cono alla corsia di marcia.
+
+        Parametri:
+            **kwargs: Dizionario di argomenti chiave variabili. I parametri attesi includono:
+            - ego_vehicle_wp (carla.Waypoint): Il waypoint corrente in cui si trova l'ego-veicolo.
+
+        Ritorna:
+            Un'azione esecutiva per il controller del veicolo (es. il richiamo a halt_vehicle() per l'arresto immediato)
+            in caso di situazioni ad alto rischio, oppure None se l'area è sicura o se l'ostacolo viene interamente gestito
+            tramite l'applicazione di un offset laterale delegato al planner locale.
+        """
         current_waypoint = kwargs.get('ego_vehicle_wp')
         if not current_waypoint:
             return None
 
         sys = self.core_system
-
-        # Scansione ambientale
         hazard_flag, hazard_obj, hazard_dist = self._detect_static_hazards(current_waypoint,
                                                                            "*static.prop.trafficwarning*")
         cone_flag, cone_obj, _ = self._detect_static_hazards(current_waypoint, "*static.prop.constructioncone*")
 
-        # Salviamo i flag nel core_system per farli leggere al NavigationCruiseEvaluator
         sys.environmental_hazards = {'tw_state': hazard_flag, 'cone_state': cone_flag}
 
         if hazard_flag is True:
